36471bfc7f4742105df6ae7c4e463f07857f6abd
eduardo3g <eduardosbrasil10@gmail.com>
2022-06-10 18:47:38 -0300

    products(infra): create initial Lambda configuration

--- lib/ecommerce-microservices-stack.ts
+++ lib/ecommerce-microservices-stack.ts
@@ -1,7 +1,13 @@
 import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EcommerceMicroservicesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -16,5 +22,23 @@ export class EcommerceMicroservicesStack extends Stack {
       removalPolicy: RemovalPolicy.DESTROY,
       billingMode: BillingMode.PAY_PER_REQUEST,
     });
+
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: ["aws-sdk"],
+      },
+      environment: {
+        PRIMARY_KEY: "id",
+        DYNAMODB_TABLE_NAME: productTable.tableName,
+      },
+      runtime: Runtime.NODEJS_14_X,
+    };
+
+    const productFunction = new NodejsFunction(this, "productLambdaFunction", {
+      entry: join(__dirname, "/../src/product/index.js"),
+      ...nodeJsFunctionProps,
+    });
+
+    productTable.grantReadWriteData(productFunction);
   }
 }
