0feb857383a946a9de4c8ac7fa05046542a283da
eduardo3g <eduardosbrasil10@gmail.com>
2022-06-10 18:53:23 -0300

    products(infra): set up API GW Rest API

--- lib/ecommerce-microservices-stack.ts
+++ lib/ecommerce-microservices-stack.ts
@@ -1,13 +1,13 @@
+import { join } from "path";
 import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
-import { join } from "path";
-
 import {
   NodejsFunction,
   NodejsFunctionProps,
 } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class EcommerceMicroservicesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -40,5 +40,20 @@ export class EcommerceMicroservicesStack extends Stack {
     });
 
     productTable.grantReadWriteData(productFunction);
+
+    const apigw = new LambdaRestApi(this, "productApi", {
+      restApiName: "Product Service",
+      handler: productFunction,
+      proxy: false,
+    });
+
+    const product = apigw.root.addResource("product");
+    product.addMethod("GET"); // GET /product
+    product.addMethod("POST"); // POST /product
+
+    const singleProduct = product.addResource("{id}"); // product/{id}
+    singleProduct.addMethod("GET"); // GET /product/{id}
+    singleProduct.addMethod("PUT"); // PUT /product/{id}
+    singleProduct.addMethod("DELETE"); // DELETE /product/{id}
   }
 }
