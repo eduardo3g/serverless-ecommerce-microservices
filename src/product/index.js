825db254a031a2260628bfc2a88b76e0d064da72
eduardo3g <eduardosbrasil10@gmail.com>
2022-06-15 14:44:51 -0300

    feat: implement the PUT/product/{id} Lambda handler

--- src/product/index.js
+++ src/product/index.js
@@ -3,6 +3,7 @@ const {
   ScanCommand,
   PutItemCommand,
   DeleteItemCommand,
+  UpdateItemCommand,
 } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 import { v4 as uuid } from "uuid";
@@ -23,6 +24,9 @@ exports.handler = async (event) => {
       break;
     case "DELETE":
       body = await deleteProduct(event.pathParameters.id);
+      break;
+    case "PUT":
+      body = await updateProduct(event.body);
       break;
     default:
       throw new Error(`Unsupported route: ${event.httpMethod}`);
@@ -110,6 +114,50 @@ const deleteProduct = async (productId) => {
     console.log(deleteResult);
 
     return deleteResult;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+};
+
+const updateProduct = async (event) => {
+  console.log(`updateProduct function. event : "${event}"`);
+
+  try {
+    const requestBody = JSON.parse(event.body);
+    const objKeys = Object.keys(requestBody);
+    console.log(
+      `updateProduct function. requestBody : "${requestBody}", objKeys: "${objKeys}"`
+    );
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ id: event.pathParameters.id }),
+      UpdateExpression: `SET ${objKeys
+        .map((_, index) => `#key${index} = :value${index}`)
+        .join(", ")}`,
+      ExpressionAttributeNames: objKeys.reduce(
+        (acc, key, index) => ({
+          ...acc,
+          [`#key${index}`]: key,
+        }),
+        {}
+      ),
+      ExpressionAttributeValues: marshall(
+        objKeys.reduce(
+          (acc, key, index) => ({
+            ...acc,
+            [`:value${index}`]: requestBody[key],
+          }),
+          {}
+        )
+      ),
+    };
+
+    const updateResult = await ddbClient.send(new UpdateItemCommand(params));
+
+    console.log(updateResult);
+    return updateResult;
   } catch (e) {
     console.error(e);
     throw e;
